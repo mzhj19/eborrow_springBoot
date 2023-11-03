0779a5a84b805bb10286f122649a7aff2a31e8e3
Md. Zahid Hasan Jamil <mzhj19@gmail.com>
2023-11-03 17:03:45 +0600

    Initial commit

---
+++ src/test/java/com/mzhj19/eborrow/EborrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzhj19.eborrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EborrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
