3c2e452547ff5fb7ef40a46e0db7bcea11eae43a
Md. Zahid Hasan Jamil <mzhj19@gmail.com>
2023-12-25 00:52:43 +0600

    Incorrect jwt token error response

--- src/main/java/com/mzhj19/eborrow/configuration/security/JwtAuthEntryPoint.java
+++ src/main/java/com/mzhj19/eborrow/configuration/security/JwtAuthEntryPoint.java
@@ -1,5 +1,8 @@
 package com.mzhj19.eborrow.configuration.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mzhj19.eborrow.data.response.ResponseErrorData;
+import com.mzhj19.eborrow.exceptions.ErrorObject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,11 +11,39 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 
 @Component
 public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
-    @Override
+    /*@Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }*/
+
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        // Set the content type of the response to application/json
+        response.setContentType("application/json");
+
+        // Set the HTTP status code to 401 (Unauthorized)
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        // Create an ErrorObject with the necessary information
+
+        ResponseErrorData<String> responseErrorData = new ResponseErrorData<String>();
+        responseErrorData.setStatus(false);
+        responseErrorData.setStatusCode(HttpServletResponse.SC_UNAUTHORIZED);
+        responseErrorData.setMessage("UNAUTHORIZED");
+
+        // Convert the ErrorObject to JSON
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writeValueAsString(responseErrorData);
+
+        // Write the JSON response to the response body
+        PrintWriter writer = response.getWriter();
+        writer.println(jsonResponse);
+        writer.flush();
     }
 }
--- src/main/java/com/mzhj19/eborrow/data/response/ResponseErrorData.java
+++ src/main/java/com/mzhj19/eborrow/data/response/ResponseErrorData.java
@@ -3,10 +3,12 @@ package com.mzhj19.eborrow.data.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ResponseErrorData<T> {
 
